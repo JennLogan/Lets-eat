c148e561c0181d9e81458247eb414c0b4463ec04
Jennifer <Angelosmami@yahoo.com>
2021-04-19 21:31:12 -0700

    m

---
+++ src/main/java/org/launchcode/letseat/LetsEatApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.letseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsEatApplication.class, args);
+	}
+
+}
